37f5bdde4b9f27a14811ad2388277c496d86cb68
Noah <vs30amef@studserv.uni-leipzig.de>
2025-11-06 17:17:15 +0100

    Hwork2 implementation still need to optimize sigmas

--- hwork2/dip2_py/dip2.py
+++ hwork2/dip2_py/dip2.py
@@ -49,6 +49,25 @@ def spatial_convolution(src: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     Performs spatial convolution of image and filter kernel.
     """
     # TO DO !!
+    flipkernel = np.flipud(np.fliplr(kernel))
+
+    src_h, src_w = src.shape
+
+    kernel_h, kernel_w = kernel.shape
+
+    padding_height = kernel_h//2
+    padding_width = kernel_w//2
+
+    padded_image = np.pad(src,((padding_height,),(padding_width,)), "reflect")
+
+    result = np.zeros_like(src, dtype=float)
+
+    for y in range(src_h):
+        for x in range (src_w):
+            region = padded_image[y:y + kernel_h, x:x+kernel_w]
+            result[y,x] = np.sum(region * flipkernel)
+
+    return result
     return np.array(src, copy=True)
 
 
@@ -58,6 +77,11 @@ def average_filter(src: np.ndarray, k_size: int) -> np.ndarray:
 
     You might want to use dip2.spatial_convolution(...) within this function.
     """
+    kernel = np.ones((k_size,k_size))
+    kernel = np.multiply(kernel, (1/(k_size*k_size)))
+
+    result = spatial_convolution(src, kernel)
+    return result
     # TO DO !!
     return np.array(src, copy=True)
 
@@ -66,7 +90,17 @@ def median_filter(src: np.ndarray, k_size: int) -> np.ndarray:
     """
     Median filter.
     """
+    padded_src = np.pad(src, ((k_size,),(k_size,)),"reflect")
+    src_h, src_w = src.shape
+    result = np.zeros_like(src, dtype=float)
+    for y in range(src_h):
+        for x in range(src_w):
+            region = padded_src[y:y+k_size , x:x+k_size]
+            region.flatten()
+            result[y,x] = np.median(region)
+            
     # TO DO !!
+    return result
     return np.array(src, copy=True)
 
 
@@ -74,7 +108,36 @@ def bilateral_filter(src: np.ndarray, k_size: int, sigma_spatial: float, sigma_r
     """
     Bilateral filter.
     """
+    padded_image = np.pad(src, ((k_size//2,k_size//2),(k_size//2,k_size//2)),"median")
+
+    result = np.zeros_like(src, dtype=float)
+    img_h, img_w = src.shape
+
+    spatialmat = np.zeros((k_size,k_size))
+    center = [k_size//2, k_size//2]
+    for y in range(k_size):
+        for x in range(k_size):
+            weight = 1/(2*np.pi*np.square(sigma_spatial))*np.exp(- (np.square(y-center[0])+np.square(x-center[1])) / (2*np.square(sigma_spatial) ))
+            spatialmat[y,x] = weight
+    
+    #print(spatialmat)
+
+    
+    for y in range (img_h):
+        for x in range (img_w):
+            r_w = np.zeros_like(spatialmat)
+            area = padded_image[y:y+k_size, x:x+k_size]
+            for ky in range(k_size):
+                for kx in range(k_size):
+                    weightrad = 1 / (2*np.pi*sigma_radiometric**2)*np.exp(
+                        - (((area[ky,kx]-area[center[0],center[1]])**2) / (2*sigma_radiometric**2))
+                    )
+                    r_w[ky,kx] = weightrad
+            final_kernel = np.multiply(spatialmat, r_w)
+            final_kernel /= np.sum(final_kernel)
+            result[y,x] = np.sum(area * final_kernel)            
     # TO DO !!
+    return result
     return np.array(src, copy=True)
 
 
@@ -90,6 +153,14 @@ def choose_best_algorithm(noise_type: NoiseType) -> NoiseReductionAlgorithm:
     Chooses the right algorithm for the given noise type.
     """
     # TO DO !!
+    match noise_type:
+        case NoiseType.NOISE_TYPE_1:
+            return NoiseReductionAlgorithm.NR_MEDIAN_FILTER
+        case NoiseType.NOISE_TYPE_2:
+            return NoiseReductionAlgorithm.NR_BILATERAL_FILTER
+        case _:
+            return NoiseReductionAlgorithm.NR_MOVING_AVERAGE_FILTER
+
     raise NotImplementedError("Student implementation missing")
 
 
@@ -105,23 +176,23 @@ def denoise_image(
 
     if noise_reduction_algorithm is NoiseReductionAlgorithm.NR_MOVING_AVERAGE_FILTER:
         if noise_type is NoiseType.NOISE_TYPE_1:
-            return average_filter(src, 1)
+            return average_filter(src, 3)
         if noise_type is NoiseType.NOISE_TYPE_2:
-            return average_filter(src, 1)
+            return average_filter(src, 3)
         raise ValueError("Unhandled noise type!")
 
     if noise_reduction_algorithm is NoiseReductionAlgorithm.NR_MEDIAN_FILTER:
         if noise_type is NoiseType.NOISE_TYPE_1:
-            return median_filter(src, 1)
+            return median_filter(src, 3)
         if noise_type is NoiseType.NOISE_TYPE_2:
-            return median_filter(src, 1)
+            return median_filter(src, 3)
         raise ValueError("Unhandled noise type!")
 
     if noise_reduction_algorithm is NoiseReductionAlgorithm.NR_BILATERAL_FILTER:
         if noise_type is NoiseType.NOISE_TYPE_1:
-            return bilateral_filter(src, 1, 1.0, 1.0)
+            return bilateral_filter(src, 13, 1.0, 100.0)
         if noise_type is NoiseType.NOISE_TYPE_2:
-            return bilateral_filter(src, 1, 1.0, 1.0)
+            return bilateral_filter(src, 13, 1.0, 100.0)
         raise ValueError("Unhandled noise type!")
 
     raise ValueError("Unhandled filter type!")
